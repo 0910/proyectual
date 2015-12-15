15c1b021f1b1fbcc7e43b06aebb221fb7d2bd106
0910 <e@nuevediez.com.ar>
2015-12-15 11:08:17 -0300

    modified db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 VideoCategory.create!(:name => 'Teaser')
 VideoCategory.create!(:name => 'Aftermovie')
 VideoCategory.create!(:name => 'Live')
