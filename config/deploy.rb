e712d87f659d99b518b96f14f8f49023f05d7632
emadobao <emadobao@gmail.com>
2016-12-14 15:55:49 -0300

    change deploy ip address

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-server '40.76.56.133', roles: [:web, :app, :db], primary: true
+server '13.92.25.71', roles: [:web, :app, :db], primary: true
 set :domain, "proyectual.nuevediez.com"
 set :repo_url,        'git@github.com:0910/proyectual.git'
 set :application,     'proyectual'
