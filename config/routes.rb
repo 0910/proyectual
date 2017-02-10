abd43e2d05e59881f9597d9c0f697e1ce7659694
0910 <e@nuevediez.com.ar>
2017-02-10 11:16:44 -0300

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   
-  scope "v2" do
-    scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/, defaults: {locale: "en"} do
-      resources :artists, only: [:show, :index]
-      resources :events, only: [:show, :index]
-      resources :news, only: [:show, :index]
-      resources :videos, only: [:index]
-      get '/about' => 'about#show', :defaults => { :id => 1 }
-      get '/v2' => 'home#v2'
-    end
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/, defaults: {locale: "en"} do
+    resources :artists, only: [:show, :index]
+    resources :events, only: [:show, :index]
+    resources :news, only: [:show, :index]
+    resources :videos, only: [:index]
+    get '/about' => 'about#show', :defaults => { :id => 1 }
+    get '/v2' => 'home#v2'
   end
 
   devise_for :admin_users, ActiveAdmin::Devise.config
