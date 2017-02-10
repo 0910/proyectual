080f846d2632c141f952dbcae943d18c2babf118
0910 <e@nuevediez.com.ar>
2017-02-10 15:18:24 -0300

    revert last routing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 class HomeController < ApplicationController
-  def index
+  def v2
     @about = About.find(1)
   	@news = News.last(2)
   	@splashes = Splash.all
   	@partners = Partner.all
   	@events = Event.where(featured: 'Yes')
+    render 'index'
+  end
+  def index
+    redirect_to "https://www.facebook.com/proyectual"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
   
-  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/, defaults: {locale: "en"} do
-    resources :artists, only: [:show, :index]
-    resources :events, only: [:show, :index]
-    resources :news, only: [:show, :index]
-    resources :videos, only: [:index]
-    get '/about' => 'about#show', :defaults => { :id => 1 }
-    root 'home#index'
+  scope "v2" do
+    scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/, defaults: {locale: "en"} do
+      resources :artists, only: [:show, :index]
+      resources :events, only: [:show, :index]
+      resources :news, only: [:show, :index]
+      resources :videos, only: [:index]
+      get '/about' => 'about#show', :defaults => { :id => 1 }
+      get '/home' => 'home#v2'
+    end
   end
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  get '/:locale' => 'home#index'
+  get '/:locale' => 'home#v2'
   get '/change_locale/:locale', to: 'settings#change_locale', as: :change_locale
-
+  root 'home#index'
 end
