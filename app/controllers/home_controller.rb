4d672b672416ec64e0c9b08d2959404ae4e3a5b6
0910 <e@nuevediez.com.ar>
2017-04-10 18:45:44 -0300

    resolve bug in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   	@news = News.all.order(date: :desc).last(2)
   	@splashes = Splash.all.order(id: :desc)
   	@partners = Partner.all
-  	@events = Event.where(featured: 'Yes').order(:year :desc)
+  	@events = Event.where(featured: 'Yes').order(year: :desc)
     set_meta_tags(
       title: "Proyetual",
       description: @about.short_about,
